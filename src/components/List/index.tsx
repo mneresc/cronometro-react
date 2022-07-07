650cf3278653a5e33dcc00bb7fdad3dbc8963d83
Neres <mneresc@gmail.com>
2022-07-07 10:56:17 -0300

    Adicionando tasks para somente testar o state

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,18 +1,21 @@
+import { useState } from 'react';
 import Item from './Item/item';
 import style from './list.module.scss'
 
 function List() {
-    const tasks = [{
+    const [tasks, setTask] = useState([{
         taskName: 'react',
         time: '01:00:00'
-    }];
+    }])
     return (
         <aside className={style.tasks}>
-            <h2>Day's studies</h2>
+            <h2 onClick={() => {
+                setTask([...tasks, { taskName: 'react', time: '01:00:00' }])
+            }}>Day's studies</h2>
             <ul>
                 {tasks.map((task, index) =>
                 (
-                    <Item key={index} taskName={task.taskName}  time={task.time} />
+                    <Item key={index} taskName={task.taskName} time={task.time} />
                 )
                 )}
             </ul>
